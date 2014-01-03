bdc013a503850235561751a31c7a18d37aa51321
Lars Moellendorf <lars@moellendorf.eu>
2014-01-03 18:44:58 +0100

    introduced string formatting
    String formatting
    =================
    
    * To fill a string with values, you use the % (modulo) operator and tuple
    (or variable/dictionary)
    
    * Each %s gets replaced with an item from the tuple, left to  right

--- src/naotutorial/speak.py
+++ src/naotutorial/speak.py
@@ -11,4 +11,4 @@ phaenao = {"name": "phänao", "ip": "192.168.1.123", "port": 9559}
     
 if __name__ == '__main__':
     tts = ALProxy("ALTextToSpeech", phaenao["ip"], phaenao["port"])
-    tts.say("Hallo, ich bin" + phaenao["name"] + "!")
+    tts.say("Hallo, ich bin %s!" % phaenao["name"])
