d212ed69231e4cb297edd27423128133c50356b9
Lars Moellendorf <lars@moellendorf.eu>
2014-01-03 16:12:54 +0100

    introduced ALTextToSpeech
    Importing libraries
    ===================
    
    External libraries are used with the
    
        import [libname]
    
    keyword. You can also use
    
        from [libname] import [funcname]
    
    for individual functions.
    
    ALProxy
    =======
    
    ALProxy is an object that gives you acces to all the methods or the
    module your are going to connect to.
    
        class ALProxy(name, ip, port)
    
    * name - The name of the module
    (https://community.aldebaran-robotics.com/doc/1-14/cpp-classindex.html)
    * ip - The IP of your robot
    * port - The port on which NAOqi listens (9559 by default)

--- src/naotutorial/speak.py
+++ src/naotutorial/speak.py
@@ -4,5 +4,8 @@ Created on 03.01.2014
 @author: lars
 '''
 
+from naoqi import ALProxy
+    
 if __name__ == '__main__':
-    print("Hallo Welt!")
+    tts = ALProxy("ALTextToSpeech", "192.168.1.123", 9559 )
+    tts.say("Hallo, ich bin phaenao!")
